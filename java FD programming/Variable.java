94f9971d10ddf494a55a8c69be690e2a348ae52f
jatinhub-c <ss8092457@gmail.com>
2025-08-11 11:01:04 +0530

    Add files via upload

---
+++ java FD programming/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a= 5;
+        int b= 5;
+        int c= a+b;
+        System.out.println(c);
+        int _$ = 69;
+        System.out.println(_$);
+    }
+}
