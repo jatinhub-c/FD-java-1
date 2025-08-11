e66c4ca548d1f335c103f429a49e8a607b272ced
jatinhub-c <ss8092457@gmail.com>
2025-08-11 11:24:07 +0530

    Add files via upload

--- java FD programming/Variable.java
+++ java FD programming/Variable.java
@@ -1,11 +1,39 @@
 public class Variable {
     public static void main(String[] args) {
         int a= 5;
-        int b= 5;
-        int sum= a+b;
-        System.out.println(sum);
+        int z= 5;
+        int m= a+z;
+        System.out.println(m);
         int _$ = 69;
         System.out.println(_$);
-    }
 
+        System.out.println(_$ + m);
+        
+         byte b = 100;                  
+        short s = 30000;               
+        int i = 100000;                
+        long l = 10000000000L;     
+
+
+        float f = 3.14f;              
+        double d = 3.14159265359;     
+
+       
+        char c = 'J';               
+        boolean bool = true;          
+
+       
+        String name = "Jatin";
+
+       
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+        System.out.println("String: " + name);
+    }
 }
